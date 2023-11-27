5aad42ad9ac7fb02b9d91be9443d3c459f1b7ee5
David Sanchez Valera <chirlo14@gmail.com>
2023-11-27 09:26:55 +0100

    icon problem solved

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
-
+import  logo from "../assets/home-icon.png"
 function Navbar() {
 
     return (
         <nav>
-            <Link to="/"><img src="./src/assets/home-icon.png" alt="home logo" width={60}/></Link>
+            <Link to="/"><img src={logo} alt="home logo" width={60}/></Link>
         </nav>
     )
 }
